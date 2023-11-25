2f04331bbc91a550fbc1ff62810dbc65e3a85371
tanjib10 <radiulmd@gmail.com>
2023-11-25 15:57:28 +0600

    added data in mongoDB database

--- index.js
+++ index.js
@@ -27,13 +27,27 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   //  await client.connect();
+
+   const brandCollection = client.db('job-cloud').collection('job-categories');
+   app.get('/job', async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   //  await client.close();
   }
 }
 run().catch(console.dir);
