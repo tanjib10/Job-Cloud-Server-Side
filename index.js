3ad57dbf4a14277e5c257af6b7e41ec3448efdb7
tanjib10 <radiulmd@gmail.com>
2023-11-27 03:52:00 +0600

    added job details by id from db

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
-const port = process.env.PORT|| 5000;
+const port = process.env.PORT || 5000;
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yfphnhu.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-   //  await client.connect();
+    await client.connect();
 
-   const brandCollection = client.db('job-cloud').collection('job-categories');
+    const brandCollection = client.db('job-cloud').collection('job-categories');
+    const bidCollection = client.db('job-cloud').collection('bids');
 
-   app.get('/job', async (req, res) => {
+       app.get('/job', async (req, res) => {
       const cursor = brandCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -45,26 +40,54 @@ async function run() {
     })
 
 
+    app.get('/job/details/:id', async (req, res) => {
+      const jobId = req.params.id;
+      const job = await brandCollection.findOne({ _id: new ObjectId(jobId) });
+      res.json(job);
+    });
+
+
+    app.post('/bid/:id', async (req, res) => {
+      const jobId = req.params.id;
+      const { price, deadline, bidderEmail } = req.body;
 
 
 
+      const job = await brandCollection.findOne({ _id: new ObjectId(jobId) });
+      if (!job) {
+        return res.status(404).json({ error: 'Job not found' });
+      }
+
+
+      if (job.ownerEmail === bidderEmail) {
+        return res.status(400).json({ error: 'Cannot bid on your own job' });
+      }
+
+      const bid = {
+        price,
+        deadline,
+        bidderEmail,
+        jobId: new ObjectId(jobId),
+      };
+
+      await bidCollection.insertOne(bid);
+      res.json({ message: 'Bid placed successfully' });
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
-    // Ensures that the client will close when you finish/error
-   //  await client.close();
+    // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-
-
-app.get('/', (req,res)=> {
-   res.send("server is running");
-})
+app.get('/', (req, res) => {
+  res.send('server is running');
+});
 
 app.listen(port, () => {
-   console.log("server is running")
-}) 
+  console.log('server is running');
+});
