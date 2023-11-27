85f245d387b9b1969c5046e4109a912e528ccfb0
tanjib10 <radiulmd@gmail.com>
2023-11-27 17:57:07 +0600

    added post method for adding jobs

--- index.js
+++ index.js
@@ -19,15 +19,22 @@ const client = new MongoClient(uri, {
   },
 });
 
+  
 async function run() {
   try {
     await client.connect();
 
-    const brandCollection = client.db('job-cloud').collection('job-categories');
+  const brandCollection = client.db('job-cloud').collection('job-categories');
     const bidCollection = client.db('job-cloud').collection('bids');
+     const newJobCollection = client.db('job-cloud').collection('new-jobs')
 
        app.get('/job', async (req, res) => {
       const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+       app.get('/job/add', async (req, res) => {
+      const cursor = newJobCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -51,28 +58,58 @@ async function run() {
       const jobId = req.params.id;
       const { price, deadline, bidderEmail } = req.body;
 
-
-
       const job = await brandCollection.findOne({ _id: new ObjectId(jobId) });
       if (!job) {
         return res.status(404).json({ error: 'Job not found' });
       }
-
-
       if (job.ownerEmail === bidderEmail) {
         return res.status(400).json({ error: 'Cannot bid on your own job' });
       }
-
       const bid = {
         price,
         deadline,
         bidderEmail,
         jobId: new ObjectId(jobId),
       };
-
       await bidCollection.insertOne(bid);
       res.json({ message: 'Bid placed successfully' });
     });
+
+
+app.post('/job/add', async (req, res) => {
+  try {
+        console.log('Received request to add a job');
+    const { ownerEmail, jobTitle, deadline, description, category, minPrice, maxPrice } = req.body;
+
+    if (!ownerEmail || !jobTitle || !deadline || !category || !minPrice || !maxPrice) {
+      return res.status(400).json({ error: 'Incomplete job information provided' });
+    }
+
+    const newJob = {
+      ownerEmail,
+      jobTitle,
+      deadline,
+      description,
+      category,
+      minPrice,
+      maxPrice,
+    };
+
+    const result = await newJobCollection.insertOne(newJob);
+
+    if (result.insertedCount === 1) {
+          console.log('Job added successfully');
+      res.status(201).json({ message: 'Job added successfully' });
+    } else {
+      res.status(500).json({ error: 'Failed to add job' });
+    }
+  } catch (error) {
+    //  console.error('Error adding job:', error);
+    console.error('Error adding job:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
